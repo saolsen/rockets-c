560383000c88db48b71374716c69bed8d66c2734
Stephen Olsen <steve.olsen@gmail.com>
2014-12-27 17:38:25 -0500

    basic structure of platform layer set up

--- sdl_platform.c
+++ sdl_platform.c
@@ -7,7 +7,6 @@ int main(int argc, char* argv[])
 {
   int screen_width = 960;
   int screen_height = 540;
-  bool running = true;
 
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     printf("Error initializing SDL: %s", SDL_GetError());
@@ -28,7 +27,41 @@ int main(int argc, char* argv[])
 
   if (!renderer) printf("Error creating renderer: %s", SDL_GetError());
 
-  SDL_Delay(1000);
+  /* Let sdl stretch our texture to the real window size. */
+  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+  SDL_RenderSetLogicalSize(renderer, screen_width, screen_height);
+
+  SDL_Texture *texture = SDL_CreateTexture(renderer,
+                                           SDL_PIXELFORMAT_ARGB8888,
+                                           SDL_TEXTUREACCESS_STREAMING,
+                                           screen_width,
+                                           screen_height);
+
+  if (!texture) printf("Error creating texture: %s", SDL_GetError());
+
+  uint32_t *pixel_buffer;
+  void *gamestate;
+
+  // Don't really know how big this is going to be yet.
+  gamestate = malloc(10000);
+  pixel_buffer = (uint32_t *) malloc(screen_width * screen_height * 4);
+
+  bool running = true;
+
+  while (running) {
+
+    //todo(stephen): event handling
+    
+    //todo(stephen): game update and render
+
+    //todo(stephen): delay the rest of the frame
+
+    //todo(stephen): update the screen
+    
+    SDL_Delay(1000);
+    break;
+    
+  }
 
   SDL_Quit();
 }
